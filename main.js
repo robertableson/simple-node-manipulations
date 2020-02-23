839cc11ff8c0066b3e6c803649f054c4d9508a36
Robert <robertableson@gmail.com>
2020-02-23 11:52:35 -0500

    Added text file read to set number to count to

--- main.js
+++ main.js
@@ -8,3 +8,24 @@ console.log('done');*/
 
 
 // count 0-100 by using text file number to count to 
+var fs = require('fs');
+
+count();
+
+function count() {
+   fs.readFile('input.txt', 'utf8', function(err, data) {
+      if (err) throw err;
+   
+      var countTo = data;
+   
+      for(var i = 0; i <= countTo; i++){
+         console.log(i);
+      }
+
+      done();
+    });
+}
+
+function done() {
+   console.log('done');
+}
